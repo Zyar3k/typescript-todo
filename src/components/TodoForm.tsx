d5713eef29a09f03eb93be339ccbe130f106d010
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-20 15:50:25 +0200

    Add handlers to form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, ChangeEvent, FormEvent} from 'react'
 
 interface TodoFormProps{
   addTodo: AddTodo;
@@ -8,9 +8,22 @@ interface TodoFormProps{
 export const TodoForm: React.FC<TodoFormProps> = ({addTodo}) => {
   const [newTodo, setNewTodo] = useState<string>("");
 
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>)=>{
+    setNewTodo(e.target.value)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLButtonElement>)=>{
+    e.preventDefault()
+    addTodo(newTodo)
+    setNewTodo('')
+  }
+
   return (
     <form  className="todo-form">
-      <input type="text" value={newTodo} placeholder='Add a todo' className="todo-input"/>
+      <input type="text" value={newTodo} placeholder='Add a todo' className="todo-input"  onChange={handleOnChange} />
+      <button type='submit' className="todo-button" onSubmit={handleSubmit}>
+        Add a todo
+      </button>
     </form>
     )
 }
